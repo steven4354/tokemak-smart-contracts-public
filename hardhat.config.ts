822e26d3c4fde88ec08a1abfa8c60571ee288c3b
kai <kai0x@outlook.com>
2022-02-28 22:38:49 -0800

    compiled the abis i needed to /artifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,9 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
+// import "@typechain/hardhat";
 // import "hardhat-gas-reporter";
-import "solidity-coverage";
+// import "solidity-coverage";
 
 dotenv.config();
 
@@ -23,7 +23,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config = {
-  solidity: "0.8.4",
+  solidity: "0.6.11",
   networks: {
     // ropsten: {
     //   url: process.env.ROPSTEN_URL || "",
